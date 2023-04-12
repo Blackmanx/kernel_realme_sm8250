59e8420137ab9d65c2045a02445369b527470e7e
Blackmanx <blackman@sajl.tk>
2023-04-12 02:11:37 +0200

    defconfig: Build dtbo

--- zip.sh
+++ zip.sh
@@ -1,4 +1,5 @@
 cp out/arch/arm64/boot/Image ./anykernel/
+cp out/arch/arm64/boot/dtbo.img ./anykernel/
 
 cd anykernel && zip -r Bigshot-kernel-$(date +"%d-%m-%Y"-%H%M).zip * && mv Bigshot-kernel-$(date +"%d-%m-%Y-%H%M").zip ../out/
 
