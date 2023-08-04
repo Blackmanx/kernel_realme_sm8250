f0a6055be8f3d0d1353bf900d47d7cedd5199ee5
EmanuelCN <emanuelghub@gmail.com>
2023-08-04 16:01:07 +0000

    kernel: sched: Revert upstream scheduler changes
        Revert "UPSTREAM: sched/topology: Allow sched_asym_cpucapacity to be disabled"
    
        Revert "sched/fair: Fix unfairness caused by missing load decay"
    
        Revert "sched/fair: Make sure to update tg contrib for blocked load"
    
        Revert "BACKPORT: sched/fair: Fix overutilized update in enqueue_task_fair()"
    
        Revert "sched/fair: Optimize select_idle_cpu"
    
        Revert "sched/fair: Fix CFS bandwidth hrtimer expiry type"
    
        Revert "sched/debug: Fix cgroup_path[] serialization"
    
        Revert "sched/deadline: Fix sched_dl_global_validate()"
    
        Revert "sched: Reenable interrupts in do_sched_yield()"
    
        Revert "UPSTREAM: sched: Fix out-of-bound access in uclamp"
    
    Reason: Performance regression

--- kernel/sched/core.c
+++ kernel/sched/core.c
@@ -782,7 +782,7 @@ DEFINE_STATIC_KEY_FALSE(sched_uclamp_used);
 
 static inline unsigned int uclamp_bucket_id(unsigned int clamp_value)
 {
-	return min_t(unsigned int, clamp_value / UCLAMP_BUCKET_DELTA, UCLAMP_BUCKETS - 1);
+	return clamp_value / UCLAMP_BUCKET_DELTA;
 }
 
 static inline unsigned int uclamp_bucket_base_value(unsigned int clamp_value)
@@ -6018,8 +6018,12 @@ static void do_sched_yield(void)
 	schedstat_inc(rq->yld_count);
 	current->sched_class->yield_task(rq);
 
+	/*
+	 * Since we are going to call schedule() anyway, there's
+	 * no need to preempt or enable interrupts:
+	 */
 	preempt_disable();
-	rq_unlock_irq(rq, &rf);
+	rq_unlock(rq, &rf);
 	sched_preempt_enable_no_resched();
 
 	schedule();
