8952ecb4b70fe995f4c037c584cbd310fe2dd757
Sultan Alsawaf <sultan@kerneltoast.com>
2024-03-12 16:55:56 -0700

    sched/cass: Only treat sync waker CPU as idle if there's one task running
    For synchronized wakes, the waker's CPU should only be treated as idle if
    there aren't any other running tasks on that CPU. This is because, for
    synchronized wakes, it is assumed that the waker will immediately go to
    sleep after waking the wakee; therefore, if there aren't any other tasks
    running on the waker's CPU, it'll go idle and should be treated as such to
    improve task placement.
    
    This optimization only applies when there aren't any other tasks running on
    the waker's CPU, however.
    
    Fix it by ensuring that there's only the waker running on its CPU.
    
    Signed-off-by: Sultan Alsawaf <sultan@kerneltoast.com>

--- kernel/sched/cass.c
+++ kernel/sched/cass.c
@@ -155,9 +155,11 @@ static int cass_best_cpu(struct task_struct *p, int prev_cpu, bool sync, bool rt
 
 		/*
 		 * Check if this CPU is idle or only has SCHED_IDLE tasks. For
-		 * sync wakes, always treat the current CPU as idle.
+		 * sync wakes, treat the current CPU as idle if @current is the
+		 * only running task.
 		 */
-		if ((sync && cpu == smp_processor_id()) || available_idle_cpu(cpu)) {
+		if ((sync && cpu == smp_processor_id() && rq->nr_running == 1) ||
+			available_idle_cpu(cpu)) {
 			/*
 			 * A non-idle candidate may be better when @p is uclamp
 			 * boosted. Otherwise, always prefer idle candidates.
