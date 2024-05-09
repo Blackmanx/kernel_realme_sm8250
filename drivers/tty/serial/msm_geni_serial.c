29fd09a5ddff937f42f749a560f8b742860075d9
Blackmanx <blackman@sajl.tk>
2024-05-10 00:55:34 +0200

    Revert "serial: msm_geni_serial: Do not enable wakeup_irq if port is closed"
    This reverts commit 47394cfc3aa842cf0769a867626f4e13e58a7763.

--- drivers/tty/serial/msm_geni_serial.c
+++ drivers/tty/serial/msm_geni_serial.c
@@ -3739,8 +3739,7 @@ static int msm_geni_serial_runtime_suspend(struct device *dev)
 		goto exit_runtime_suspend;
 	}
 
-	/* Check if port is in open state before enabling wakeup_irq */
-	if (port->wakeup_irq > 0  && port->uport.state->port.tty) {
+	if (port->wakeup_irq > 0) {
 		port->edge_count = 0;
 		enable_irq(port->wakeup_irq);
 	}
