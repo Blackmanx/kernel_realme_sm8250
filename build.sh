1790f32b65dd658f4f132b2f19af19f49f63b174
JingBuzz <119687768+Jeinse@users.noreply.github.com>
2025-08-10 03:32:04 +0800

    Add files via upload

--- build.sh
+++ build.sh
@@ -8,10 +8,10 @@ KERNEL_DEFCONFIG=vendor/sm8250_defconfig
 DIR=$PWD
 export ARCH=arm64
 export SUBARCH=arm64
-export CLANG_PATH="~/toolchains/bin"
+export CLANG_PATH="/home/wenxuan/clang/clang3399163b/bin"
 export PATH="$CLANG_PATH:$PATH"
-export CROSS_COMPILE=aarch64-linux-gnu-
-export CROSS_COMPILE_ARM32=arm-linux-gnueabi-
+export CROSS_COMPILE=/home/wenxuan/gcc/gcc-arm-10.3-2021.07-x86_64-aarch64-none-linux-gnu/bin/aarch64-none-linux-gnu-
+export CROSS_COMPILE_ARM32=/home/wenxuan/gcc/gcc-arm-11.2-2022.02-x86_64-arm-none-linux-gnueabihf/bin/arm-none-linux-gnueabihf-
 export KBUILD_BUILD_USER=Amog
 export KBUILD_BUILD_HOST=Us
 
